0a9b4c0b9a46029e43539baf3434ad553f4072f8
ksheerashetty <65451950+Ksheerashetty@users.noreply.github.com>
2023-08-28 20:33:56 +0530

    Create AreaOfRectangle.java
    Find Area of Rectangle
    Problem Statement:
    Take the length(L) and breadth(B) of the rectangle as input and find its area.
    
    Sample Input 1:
    4 20
    Sample Output 1:
    80

---
+++ Basics of programming/AreaOfRectangle.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Solution {
+	
+	public static void main(String args[]) {
+		
+		// Write code here
+		int l,b;
+		Scanner s=new Scanner(System.in);
+		l=s.nextInt();
+		b=s.nextInt();
+		System.out.println(l*b);
+		
+	}
+}
